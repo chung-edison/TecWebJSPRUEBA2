10af522828a1c9477d29f70f061fd2ec114154bb
chung-edison <eddy2809@hotmail.com>
2017-02-26 10:10:07 -0500

    sails new Bodegas + apis

---
+++ Bodegas/api/models/Bodega.js
@@ -0,0 +1,21 @@
+/**
+ * Bodega.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: string,
+    direccion: string,
+    capacidadEnToneladas: integer,
+    items: {
+      collection: "Item",
+      via: "idBodega"
+    }
+  }
+
+};
+
